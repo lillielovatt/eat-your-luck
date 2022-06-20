d80adfea8b6e3bf1cf4079aca8b0fd268e250cf7
lillielove <lillielove@gmail.com>
2022-06-19 17:51:55 -0700

    added recipe information to final page, including ingredient table, instructions, adding to previous name and photo. Still need to consider adding youtube link href to title of dish, and figure out instructions looking better (starting new lines, etc), as well as tailwind table being better formatted

---
+++ assets/js/notes.js
@@ -0,0 +1,76 @@
+
+// // var foodDisplayEl = document.querySelector("#restart-game");
+// // foodDisplayEl.addEventListener("click", highOrLow);
+
+
+
+
+
+// // Access to fetch at 'https://random-d.uk/api/v2/random' from origin 'null' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource. If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled.
+
+
+
+
+
+
+
+
+// // function getDrink() {
+// //     while (currentPageEl.firstChild) {
+// //         currentPageEl.removeChild(currentPageEl.lastChild);
+// //     }
+// //     var apiUrl = "https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegan";
+// // }
+
+
+// // if(pickCardEl){
+// //     console.log(11);
+// //     pickCardEl.addEventListener("click", pickCard);
+// // }
+
+// // click element--when you click on one of the display cards, then you
+
+// // if you picked the lower of the two cards, then--drink
+// // if you picked the higher of the two cards, then--dessert
+// // if the two cards are the same, then--tell them the odds of that
+
+// // display button to head back to the beginning
+
+
+
+// modal - the idea of this website is just --see where your luck takes you. Are you gonna get lucky, or unlucky? Dessert, or vegan?
+
+
+
+//     console.log(12);
+    //     // console.log(event.target);
+    //     // var ted=event.target;
+    //     // console.log(ted[38]);
+    //     // console.log(event.target.className);
+
+    //     // "https://deckofcardsapi.com/static/img/AH.svg"
+    //     // var cardSelected = event.target.className;
+    // console.log("high card")
+
+    // put here insead of inside getfood/getdrink?
+
+    // while (currentPageEl.firstChild) {
+    //     currentPageEl.removeChild(currentPageEl.lastChild);
+    // }
+
+
+
+    // dynamically create HTML, that shows two rectangles that are opaque/hidden
+
+    // spade - https://svgsilh.com/svg/145116.svg
+
+    // id="${cardOneValue}" id="${cardTwoValue}"
+    // makes it so that inside of the div for the class
+    // var cardOneValue = cardOne.substring(0, cardOne.length - 1);
+    // var cardTwoValue = cardTwo.substring(0, cardTwo.length - 1);
+
+    // here I can say which is high and which is low. 
+
+
+
+    
